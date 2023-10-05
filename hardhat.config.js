13978eb3b50e89c29c5690ddb0046e921e906df9
Abdul Fatir Ansari <abdulfatiransari2@gmail.com>
2023-10-05 22:48:12 +0500

    fix: hot fixes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
@@ -7,5 +8,25 @@ require("./tasks/faucet");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: {
+        version: "0.8.9",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 100000,
+            },
+        },
+    },
+    networks: {
+        hardhat: {},
+        mumbai: {
+            url: "https://rpc-mumbai.maticvigil.com",
+            // crypto wallet private key
+            accounts: [`0x${process.env.PRIVATE_KEY}`],
+        },
+    },
+    etherscan: {
+        // chain apikey like (polygon, bsc, ethereum)
+        apiKey: process.env.API_KEY,
+    },
 };
