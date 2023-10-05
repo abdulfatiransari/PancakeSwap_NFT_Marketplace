fcd2d3a2b4c0a9fc708003e3804c7421f4d3e07b
Abdul Fatir Ansari <abdulfatiransari2@gmail.com>
2023-10-05 22:48:12 +0500

    fix: hot fixes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,58 +4,62 @@
 const path = require("path");
 
 async function main() {
-  // This is just a convenience check
-  if (network.name === "hardhat") {
-    console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
+    // This is just a convenience check
+    if (network.name === "hardhat") {
+        console.warn(
+            "You are trying to deploy a contract to the Hardhat Network, which" +
+                "gets automatically created and destroyed every time. Use the Hardhat" +
+                " option '--network localhost'"
+        );
+    }
+
+    // ethers is available in the global scope
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying the contracts with the account:", await deployer.getAddress());
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    // const Token = await ethers.getContractFactory("Token");
+    // const token = await Token.deploy();
+    // await token.deployed();
+
+    const Token = await ethers.getContractFactory("ERC721NFTMarketV1");
+    const token = await Token.deploy(
+        "0x4DA21707a86F29033F26c0adBd70E9D105299467",
+        "0x4DA21707a86F29033F26c0adBd70E9D105299467",
+        "0x0FD23FA2AADC9BA7A31aA0d948ceCCE74f827203",
+        "1",
+        "100"
     );
-  }
+    await token.deployed();
 
-  // ethers is available in the global scope
-  const [deployer] = await ethers.getSigners();
-  console.log(
-    "Deploying the contracts with the account:",
-    await deployer.getAddress()
-  );
+    console.log("Token address:", token.address);
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
-
-  console.log("Token address:", token.address);
-
-  // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
+    // We also save the contract's artifacts and address in the frontend directory
+    saveFrontendFiles(token);
 }
 
 function saveFrontendFiles(token) {
-  const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+    const fs = require("fs");
+    const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
 
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
-  }
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
 
-  fs.writeFileSync(
-    path.join(contractsDir, "contract-address.json"),
-    JSON.stringify({ Token: token.address }, undefined, 2)
-  );
+    fs.writeFileSync(
+        path.join(contractsDir, "contract-address.json"),
+        JSON.stringify({ Token: token.address }, undefined, 2)
+    );
 
-  const TokenArtifact = artifacts.readArtifactSync("Token");
+    const TokenArtifact = artifacts.readArtifactSync("Token");
 
-  fs.writeFileSync(
-    path.join(contractsDir, "Token.json"),
-    JSON.stringify(TokenArtifact, null, 2)
-  );
+    fs.writeFileSync(path.join(contractsDir, "Token.json"), JSON.stringify(TokenArtifact, null, 2));
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
